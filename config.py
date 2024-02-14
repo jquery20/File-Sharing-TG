02924f14c3bb2959bc14a76aea4e2a01c20bd5db
jquery20 <96495928+jquery20@users.noreply.github.com>
2024-02-14 05:40:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ DB_NAME = environ.get("DB_NAME", "mjbotz")
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_USERNAME = environ.get("BOT_USERNAME", "") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/51ec365f25db9a7b80000.png https://graph.org/file/51ec365f25db9a7b80000.png')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
